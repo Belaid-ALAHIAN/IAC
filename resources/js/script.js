1a01629225ced4861d25e6b76c51a5d63b53bc0a
Belaid Alahian <alahian.belaid@gmail.com>
2020-02-11 22:04:25 +0100

    first commit

---
+++ resources/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.book-list').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+
+
+    
+
+});
+
+
