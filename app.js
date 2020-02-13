f8d809407b1b8a1ff883cbba12fe51b4943058a7
Belaid Alahian <alahian.belaid@gmail.com>
2020-02-13 20:08:44 +0100

    new folders

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const path = require('path');
 
 const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+
+app.use(express.static(path.join(__dirname,'public')));
 
 
 
